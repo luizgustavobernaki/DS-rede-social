47c171d979c3ba4854cebb3014e78514fd9e9271
Luiz Gustavo Bernaki <103518504+luizgustavobernaki@users.noreply.github.com>
2024-09-25 08:26:53 -0300

    js alerações

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -2,7 +2,17 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarinformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-
+<span>&{pessoasNoMundo}bilhões</span>
 }
 
 const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container_texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}
+bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas}bilhões</span>
+estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas
+<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoaas estão conectadas em alguma rede social.`
+// template string
+
+const container = document.getElementByld('graficos-container')
+container.appendChild(paragrafo)
+
