39065affb972cefcc79d3d19796310ab14286011
Luiz Gustavo Bernaki <103518504+luizgustavobernaki@users.noreply.github.com>
2024-09-25 08:45:41 -0300

    alterações

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,6 +3,11 @@ async function visualizarinformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
 }
+const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+const horas = perseInt(dados.tempo_medio)
+const minutos = Math.round((dados.tempo_medio-horas) * 100)
+const porcentagemConectada = ((pessoasconectadas / pessoasNoMundo) * 100).toFixed(2)
 
 const paragrafo = document.createElement('p')
 paragrafo.classList.add('graficos-container_texto')
@@ -15,11 +20,6 @@ estão conectadas em alguma rede social e passam em média <span>${horas} horas<
 const container = document.getElementByld('graficos-container')
 container.appendChild(paragrafo)
 
-const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
-const horas = perseInt(dados.tempo_medio)
-const minutos = Math.round((dados.tempo_medio-horas) * 100)
-const porcentagemConectada = ((pessoasconectadas / pessoasNoMundo) * 100).toFixed(2)
 
 console.log(dados); 
 
