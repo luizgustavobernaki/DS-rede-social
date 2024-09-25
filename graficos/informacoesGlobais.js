6108f7a0e75910b0828e118d980a7e6d8a5430b1
Luiz Gustavo Bernaki <103518504+luizgustavobernaki@users.noreply.github.com>
2024-09-25 08:36:41 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -9,7 +9,7 @@ paragrafo.classList.add('graficos-container_texto')
 paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}
 bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas}bilhões</span>
 estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas
-<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoaas estão conectadas em alguma rede social.`
+<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 // template string
 
 const container = document.getElementByld('graficos-container')
