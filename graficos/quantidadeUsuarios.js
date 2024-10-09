ab34f255f0dab7d437d31136bca1da45ad88b5d7
Luiz Gustavo Bernaki <103518504+luizgustavobernaki@users.noreply.github.com>
2024-10-09 08:03:27 -0300

    alterações

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,7 +1,7 @@
 import { getCSS } from "./common.js";
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
+    const res = await fetch (url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
